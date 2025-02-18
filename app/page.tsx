de2b8fde1d252ca6ed06a55176e25467b38a5699
Suzan Al Majdalawi <suzanmajdalawi1@gmail.com>
2025-02-18 09:13:47 +0100

    .

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 /*"use client";
-import { useState } from "react";*/
+import { useState } from "react";  {useEffect}*/
 
 import { fetchProduct } from "@/actions";
 import styles from "./page.module.css";
@@ -7,7 +7,9 @@ import { CardList } from "@/components/product-cards/cards";
 
 export default async function Home() {
   const { products } = await fetchProduct();
- /*const [name, setName] = useState<string>("Suzan");      <h1>Hello {name}</h1>*/
+ /*const [name, setName] = useState<string>("Suzan");  
+     <h1>Hello {name}</h1>
+  const [error, setError] = useState<string | null>(null);*/
   return (
     <div className={styles.page}>
       <main className={styles.main}>
